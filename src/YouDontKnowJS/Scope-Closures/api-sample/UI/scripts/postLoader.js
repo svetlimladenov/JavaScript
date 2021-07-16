3719cf577b15452d38b90d7464d5ca15d2c7ec64
Svetlin <svetlimladenov@gmail.com>
2021-07-16 19:57:39 +0300

    Style the api-sample code

--- src/YouDontKnowJS/Scope-Closures/api-sample/UI/scripts/postLoader.js
+++ src/YouDontKnowJS/Scope-Closures/api-sample/UI/scripts/postLoader.js
@@ -3,40 +3,38 @@
 
 const PostLoader = (($, utils) => {
     const postApi = {
-        getAllPosts : {
+        getAllPosts: {
             type: "GET",
-            url: "https://localhost:5001/Post"
-        }
-    }
+            url: "https://localhost:5001/Post",
+        },
+    };
 
     let cachedPosts = {}; // This is a hidden property
     let useCacheSetting = true;
 
     const loadAll = function loadAll(callBack, useCache = useCacheSetting) {
-        if(useCache && !utils.isEmpty(cachedPosts)) { // we can access it only via the closure of the Module
+        if (useCache && !utils.isEmpty(cachedPosts)) {
+            // we can access it only via the closure of the Module
             callBack(cachedPosts);
             console.log("using cache");
-            return; 
+            return;
         }
 
-        $.ajax(postApi.getAllPosts)
-            .done((data) => {
-                cachedPosts = data;
-                callBack(data);
-            });
-    }
+        $.ajax(postApi.getAllPosts).done((data) => {
+            cachedPosts = data;
+            callBack(data);
+        });
+    };
 
-    const load = function load(id) {
-        
-    }
+    const load = function load(id) {};
 
     const toggleCache = function toggleCache() {
         useCacheSetting = !useCacheSetting;
-    }
+    };
 
     return {
         loadAll,
         toggleCache,
-        load
-    }
+        load,
+    };
 })(jQuery, Utils);
--- src/YouDontKnowJS/Scope-Closures/api-sample/UI/scripts/utils.js
+++ src/YouDontKnowJS/Scope-Closures/api-sample/UI/scripts/utils.js
@@ -1,11 +1,14 @@
+// eslint-disable-next-line no-unused-vars
 const Utils = (() => {
     return {
         isEmpty(obj) {
-            for(var key in obj) {
-                if(obj.hasOwnProperty(key))
+            // eslint-disable-next-line no-restricted-syntax
+            for (const key in obj) {
+                if (obj.hasOwnProperty(key)) {
                     return false;
+                }
             }
-            return true;        
-        }
-    }
+            return true;
+        },
+    };
 })();
