490b36b5363809df31339f4ef2107ae161585cfe
Svetlin <svetlimladenov@gmail.com>
2021-08-05 22:51:29 +0300

    Add simple class vs function class example

---
+++ src/YouDontKnowJS/this-ObjectPrototypes/es6Classes/v8Dev.js
@@ -0,0 +1,41 @@
+/* eslint-disable max-classes-per-file */
+class A {}
+A.prototype.x = 100;
+
+class B extends A {
+    m() {
+        return super.x;
+    }
+}
+
+const b = new B();
+console.log(b.m());
+
+console.log(Object.getPrototypeOf(b) === B.prototype);
+console.log(Object.getPrototypeOf(B.prototype) === A.prototype);
+console.log(Object.getPrototypeOf(B) === A);
+
+function Foo() {}
+Foo.prototype.x = 100;
+
+function Bar() {}
+Bar.prototype = Object.create(Foo.prototype);
+Bar.prototype.m = function () {
+    return this.x;
+};
+
+const c = new Bar();
+console.log(c.m());
+
+console.log(Object.getPrototypeOf(c) === Bar.prototype);
+console.log(Object.getPrototypeOf(Bar.prototype) === Foo.prototype);
+console.log(Object.getPrototypeOf(Bar) === Foo);
+
+const myproto = {
+    __proto__: { x: 100 },
+    m() {
+        return super.x;
+    }
+};
+const o = { __proto__: myproto };
+o.m(); // returns 100
